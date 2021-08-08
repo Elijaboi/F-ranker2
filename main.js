44ebb2432fce394295c8f0a671216d569b6f34b7
Elijaboi <elijahsb777@gmail.com>
2021-08-08 12:18:22 +0300

    5

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-Moralis.initialize("INSERT_APP_ID"); // Application id from moralis.io
-Moralis.serverURL = "INSERT_SERVER_URL"; //Server url from moralis.io
+Moralis.initialize("L2xaZjXsySR9IHB6IND1FHaCK2cuK0wVvXA9yNdF"); // Application id from moralis.io
+Moralis.serverURL = "https://xvndizkxt57v.moralisweb3.com:2053/server"; //Server url from moralis.io
 
-async function login() {
+async function init() {
     try {
-        user = await Moralis.Web3.authenticate();
-        console.log(user);
-        alert("User logged in")
+        let user = Moralis.User.current();
+        if(!user){ 
+            $("#login_button").click(async ()=> {
+                user = await Moralis.Web3.authenticate();
+            })
+        }
+        renderGame();
     } catch (error) {
         console.log(error);
     }
 }
 
-document.getElementById("login_button").onclick = login;
+function renderGame(){
+    $("#game").show();
+    $("#login_button").hide();
+    $("#logo1").hide(); /*change this when routing*/
+
+}
+
+init();
+
+
