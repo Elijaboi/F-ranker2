7f632c129f6246a3b8cc6d6c7a507a644bae14d8
Elijaboi <elijahsb777@gmail.com>
2021-10-11 22:56:52 +0300

    12

--- main.js
+++ main.js
@@ -35,25 +35,34 @@ function renderGame(){
 
 }
 
-async function NFTMarket(){
-  $("#game").hide();
-  $("#login_button").hide();
-  $("#logo1").hide(); /*change this when routing*/
-  const options = { address: "0x918e8776743aaa9e04ea2fb6bb50baa11ee4c28b", chain: "rinkeby" };
-  let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
-  renderInventory(NFTs);
-  
-  console.log(NFTs);
-  //fetchNFTMetadata(NFTs);
-  
-
-
-}
-
-function renderInventory(NFTs){
-  const parent =document.getElementById("app1")
+function fetchNFTMetadata(NFTs){
+  $("#btn-logout").show(); 
+  let promises = [];
   for (let i = 0; i < NFTs.length; i++) {
-    const element = NFTs[i];
+    let nft = NFTs[i];
+    let id = nft.token_id;
+  // call moralis clous fn ->static JSON file
+
+promises.push(
+  fetch("https://cr7ge1kve9u7.moralishost.com:2053/server/functions/getNFT?_ApplicationId=EcXFhXVLhnGqUZNx9h8IyhtbZfwZCHYKbyIIlcZ3&nftId=" + id)
+// then(res => res.json())
+//then(res => JSON.parse(res.result))
+.then(res => res.json())
+//.then(res => JSON.parse(res.result))
+.then(res => console.log(res.result.data))
+.then(res =>(nft.metadata = res))
+.then(()=>{return nft;}))
+    
+  }
+  return Promise.all(promises);
+}
+function renderInventory(NFTs){
+  
+  const parent =document.getElementById("app1");
+  
+  for (let i = 0; i < NFTs.length; i++) {
+    console.log("renderinventy");
+    let nft = NFTs[i];
     let htmlString = `<div class="card" style="width: 18rem;">
     <img class="card-img-top" src="${nft.metadata.image}" alt="Card image cap">
     <div class="card-body">
@@ -65,21 +74,35 @@ function renderInventory(NFTs){
   let col = document.createElement("div");
   col.className = "col col-md-3";
   col.innerHTML = htmlString;
+  
+  parent.appendChild(col);
     
   }
 }
+async function NFTMarket(){
+  $("#game").hide();
+  $("#login_button").hide();
+  $("#logo1").hide(); /*change this when routing*/
+  
+  const options = { address: "0x918e8776743aaa9e04ea2fb6bb50baa11ee4c28b", chain: "rinkeby" };
+  let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
+//const tokenMetadata = await Moralis.Web3API.token.getTokenMetadata(options);
+ // const tokenMetadata = await Moralis.Web3API.token.getTokenIdMetadata({ address: "0x918e8776743aaa9e04ea2fb6bb50baa11ee4c28b",token_id: "1", chain: "rinkeby" })
+  
+  console.log(NFTs);
+  ///console.log(tokenMetadata.result);
+  fetchNFTMetadata(NFTs.result);
+  $("#NFTdisp").show(); 
+  renderInventory(NFTs);
+  
+  
 
-//function fetchNFTMetadata(NFTs){
-  //for (let i = 0; i < NFTs.length; i++) {
-    //let nft = NFTs[i];
-    //let id = nft.token_id;
-    //call moralis clous fn ->static JSON file
 
-//fetch("https://cr7ge1kve9u7.moralishost.com:2053/server/functions/getNFT?_ApplicationId=EcXFhXVLhnGqUZNx9h8IyhtbZfwZCHYKbyIIlcZ3&nftId=" + id)
-//.then(res =>(console.log(res)));
-    
-  //}
-//}
+}
+
+
+
+
 
 init();
 
